4f4e4b1a536f08781afbe61754f4c5b812274130
aditihaiman <ahaiman00@stuy.edu>
2019-11-26 15:06:17 -0500

    finished

--- parse.c
+++ parse.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 
 char ** parse_args( char * line ) {
-    char * args[5];
+    char ** args = malloc(5 * sizeof(char *));
     char *s1 = line;
     int idx = 0;
     char *s;
@@ -12,19 +12,13 @@ char ** parse_args( char * line ) {
         args[idx] = strsep( &line, " ");
         idx++;
     }
-    printf("0: %s\n", args[0]);
-    printf("1: %s\n", args[1]);
-    printf("2: %s\n", args[2]);
+    args[idx] = NULL;
     return args;
 }
 int main() {
     char line[100] = "ls -a -l";
     char *s1 = line;
-    //char *s2 = strsep( &s1, "-" );
-    //printf("[%s]\n", s2);
-    //printf("[%s]\n", s1);
     char ** args = parse_args(s1);
-
-    
+    execvp("ls", args);
     return 0;
 }
